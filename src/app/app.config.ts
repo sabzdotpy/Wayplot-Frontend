a35a06a1652e8508ecc2e88121449c5e0638f8ce
Sabari S <incrediblesabari02@gmail.com>
2025-12-05 11:50:10 +0530

    firebase project added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,13 +5,15 @@ import { provideHttpClient } from '@angular/common/http';
 
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(), provideFirebaseApp(() => initializeApp({ projectId: "wayplot-trioverse", appId: "1:1098178042986:web:f3ede9a77988864a987e13", storageBucket: "wayplot-trioverse.firebasestorage.app", apiKey: "AIzaSyAxPI_F_6a0hccY8MZsKSYuEmoBNpltr3k", authDomain: "wayplot-trioverse.firebaseapp.com", messagingSenderId: "1098178042986", measurementId: "G-2PK222CVXR", projectNumber: "1098178042986", version: "2" })), provideAuth(() => getAuth()),
        
   ]
 };
